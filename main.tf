58e5f49816b5fa48899b9dc7b3cab2e94f387d72
gu4ripolo <el_fackoo@hotmail.com>
2022-02-22 18:20:58 -0600

    Fixing variables

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ resource "azurerm_virtual_network" "aks_vnet" {
   name                = var.vnet_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  address_space       = var.address_space
+  address_space       = ["10.0.0.0/8"]
 }
 
 resource "azurerm_subnet" "aks_subnet" {
   name                 = var.subnet_name
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.vnet_name
-  address_prefixes     = [var.address_prefixes]
+  address_prefixes     = ["10.240.0.0/16"]
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,4 @@
 variable "vnet_name" {}
 variable "location" {}
-variable "address_space" {}
 variable "resource_group_name" {}
 variable "subnet_name" {}
-variable "address_prefixes" {}
